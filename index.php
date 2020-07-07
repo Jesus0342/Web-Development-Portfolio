3889a33495864753011be07a4ccf81fa26a66344
Jesus Sanchez <j.sanchez0342@gmail.com>
2020-07-06 20:52:00 -0700

    started about me section

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>Jesus Sanchez | Web Development Portfolio</title>
+</head>
+<body>
+    <nav>
+        <div class="heading-wrapper">
+
+        </div>
+        
+        <ul class="nav">
+            <a class="nav-item" href="#welcome"><li>Welcome</li></a>
+            <a class="nav-item" href="#projects"><li>Projects</li></a>
+            <a class="nav-item" href="#contact-me"><li>Contact Me</li></a>
+        </ul>
+    </nav>
+
+    <div class="sections-wrapper">
+        <section id="welcome">
+            <div id="welcome-content" class="section-content">
+                <div id="profile-pic-wrapper">
+                    <div id="profile-pic" class="circle">
+                        <img class="circle" src="img/jesus.jpg" alt="Profile Picture">
+                    </div>
+                </div>
+
+                <div id="about-me-wrapper">
+                    <div id="name-wrapper">
+                        <h1 id="first-name">Jesus</h1>
+                        <h1 id="last-name">Sanchez</h1>
+                    </div>
+
+                    <h2 id="job-title">Full Stack Web Developer</h2>
+
+                    <div id="link-wrapper">
+                        <a class="circle my-links" href="files/anniversary.png" download><i class="fa fa-download" aria-hidden="true"></i></a>
+                        <a class="circle my-links" href="https://github.com/Jesus0342" target="blank"><i class="fa fa-github-alt"></i></a>
+                        <a class="circle my-links" href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="projects">
+            <div id="projects-content" class="section-content">
+
+            </div>
+        </section>
+    </div>
+</body>
+</html>
